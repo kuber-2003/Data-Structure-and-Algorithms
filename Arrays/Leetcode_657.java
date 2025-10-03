1fd7aabed43a3b467f12b7772b11d954366074ed
kuber-2003 <kuberbansal26@gmail.com>
2025-10-03 20:06:42 +0530

    Rename

---
+++ Arrays/Leetcode_657.java
@@ -0,0 +1,28 @@
+package Arrays;
+
+public class Leetcode_657 {
+    public static void main(String[] args) {
+        String moves = "LL";
+        System.out.println(judgeCircle(moves));
+
+    }
+
+        public static boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+
+        for (int i = 0; i < moves.length(); i++) {
+            char ch = moves.charAt(i);
+
+            if (ch == 'U')
+                y++;
+            else if (ch == 'D')
+                y--;
+            else if (ch == 'L')
+                x--;
+            else if (ch == 'R')
+                x++;
+        }
+
+        return (x == 0 && y == 0);
+    }
+}
