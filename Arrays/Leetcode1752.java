95a4299dd7f1d75119d1caffbede1a645555359d
kuber-2003 <kuberbansal26@gmail.com>
2025-10-17 23:54:49 +0530

    Leetcode1752

---
+++ Arrays/Leetcode1752.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class Leetcode1752 {
+    public static boolean check(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+        for (int i = 1; i < n; i++) {
+            if (nums[i] < nums[i - 1]) {
+                count++;
+            }
+        }
+        if (nums[n - 1] > nums[0]) {
+            count++;
+        }
+        return count <= 1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 4, 5, 1, 2};
+        System.out.println(check(nums));
+    }
+}
