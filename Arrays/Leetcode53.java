76334ad301f8ea551b058a8d86aa066533312de4
kuber-2003 <kuberbansal26@gmail.com>
2025-10-24 23:32:12 +0530

    Leetcode53

---
+++ Arrays/Leetcode53.java
@@ -0,0 +1,23 @@
+package Arrays;
+
+class Leetcode53 {
+    public static int maxSubArray(int[] nums) {
+        int sum = 0;
+        int maxi = nums[0];
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            if (sum > maxi) {
+                maxi = sum;
+            }
+            if (sum < 0) {
+                sum = 0;
+            }
+        }
+        return maxi;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+        System.out.println("Maximum Subarray Sum: " + maxSubArray(nums));
+    }
+}
