8eb068bcc3ebf05e65f63f981c33df94fb5638d8
kuber-2003 <kuberbansal26@gmail.com>
2025-11-01 22:50:46 +0530

    leetcode15

---
+++ Arrays/Leetcode15.java
@@ -0,0 +1,41 @@
+package Arrays;
+import java.util.*;
+
+class Leetcode15 {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for (int i = 0; i < n; i++) {
+            if (i != 0 && nums[i] == nums[i - 1])
+                continue;
+
+            int j = i + 1;
+            int k = n - 1;
+
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
+
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
+                } else {
+                    ans.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+
+                    while (j < k && nums[j] == nums[j - 1]) j++;
+                    while (j < k && nums[k] == nums[k + 1]) k--;
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        System.out.println(threeSum(nums));
+    }
+}
