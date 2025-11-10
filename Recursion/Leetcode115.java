a9f62c8f61b139de31e7d39dfc8e1ec4a3304a5e
kuber-2003 <kuberbansal26@gmail.com>
2025-11-10 23:41:01 +0530

    Leetcode115

---
+++ Recursion/Leetcode115.java
@@ -0,0 +1,22 @@
+package Recursion;
+
+class Leetcode115 {
+    public static boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("[^a-z0-9]", "");
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        String str = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(str)); 
+    }
+}
