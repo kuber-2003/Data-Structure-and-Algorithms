9a39c740eaba4a6853bcc882ab97cebd57da3399
kuber-2003 <kuberbansal26@gmail.com>
2025-11-29 23:51:19 +0530

    leetcode33

---
+++ BinarySearch/Leetcode33.java
@@ -0,0 +1,41 @@
+package BinarySearch;
+
+public class Leetcode33 {
+
+    public static int search(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+
+        while (low <= high) {
+            int mid = (low + high) / 2;
+
+            if (nums[mid] == target)
+                return mid;
+
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else {
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 0;
+
+        int result = search(nums, target);
+        System.out.println("Index: " + result);
+    }
+}
